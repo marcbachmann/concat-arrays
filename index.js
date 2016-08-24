ba3c84aca63c8f7296f171d9c744e54851726039
Marc Bachmann <marc.brookman@gmail.com>
2016-08-24 21:29:03 +0200

    Add standard

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function concatArrays () {
   var index = 0
 
   for (var a = 0, alen = arguments.length; a < alen; a++) {
-      array = arguments[a]
-      if (!(array && array.length)) continue
-      for (var b = 0, blen = array.length; b < blen; b++) {
-          concatenated[index++] = array[b]
-      }
+    array = arguments[a]
+    if (!(array && array.length)) continue
+    for (var b = 0, blen = array.length; b < blen; b++) {
+      concatenated[index++] = array[b]
+    }
   }
 
   return concatenated
